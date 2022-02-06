d398488bfc183227483c2d8736c12b0def747aa5
ErickNot <75856179+ErickNotFound@users.noreply.github.com>
2022-02-06 14:48:26 -0300

    06/02

---
+++ RdxBrowser/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// O modelo de item de Página em Branco está documentado em https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x416
+
+namespace RdxBrowser
+{
+    /// <summary>
+    /// Uma página vazia que pode ser usada isoladamente ou navegada dentro de um Quadro.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void BackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (WebBrowser.CanGoBack)
+            {
+                WebBrowser.GoBack(); 
+            }
+        }
+
+        private void FrdBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (WebBrowser.CanGoForward)
+            {
+                WebBrowser.GoForward(); 
+            }
+        }
+
+        private void Buscador_KeyDown(object sender, KeyRoutedEventArgs e)
+        {
+            if(e.Key == Windows.System.VirtualKey.Enter)
+            {
+                Search();
+            }
+        }
+
+        private void Search()
+        {
+            WebBrowser.Source = new Uri("https://www.google.com/search?q=" + Buscador.Text);
+        }
+    }
+}
